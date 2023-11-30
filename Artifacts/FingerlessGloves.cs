822d227d70aa9f6718b83b2f1ce61099a5be6de5
Mezzelo <zemelozzelo@gmail.com>
2023-11-30 08:29:24 -0500

    final final lmao

--- Artifacts/FingerlessGloves.cs
+++ Artifacts/FingerlessGloves.cs
@@ -4,7 +4,7 @@
     public class FingerlessGloves : Artifact {
         public int counter = 0;
         public override string Description() => "Whenever you play 5 cards in a single turn, gain 1 <c=status>EVADE</c>.";
-        public override int? GetDisplayNumber(State s) => s.route is Combat ? counter : null;
+        public override int? GetDisplayNumber(State s) => s.route is Combat && counter < 5 ? counter : null;
         public override void OnPlayerPlayCard(int energyCost, Deck deck, Card card, State state, Combat combat, int handPosition, int handCount) {
             counter++;
             if (counter == 5) {
